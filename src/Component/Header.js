fe8b3457573c3554d5fff74ea105dc48529c0d86
AdeyemiAdekunle <bamidek@gmail.com>
2022-10-31 18:07:21 +0100

    update the authentication login

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -4,13 +4,28 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <>
-      <header className='header'>
-        <nav className='navbar'>
-          <Link to="/" end='true' className='link'>
-            Home
-          </Link>
-          <Link to="/errorboundary" className='link'>ErrorBoundary</Link>
-          <Link to="/pagination" className='link'>Pagination</Link>
+      <header className="header">
+        <nav  className='nav_container'>
+          <div className="navbar">
+            <Link to="/" end="true" className="link">
+              Home
+            </Link>
+            <Link to="/errorboundary" className="link">
+              ErrorBoundary
+            </Link>
+            <Link to="/pagination" className="link">
+              Pagination
+            </Link>
+          </div>
+
+          <div className='nav_button'>
+            <div>
+              <button>Sign In with Google</button>
+            </div>
+            <div>
+              <button>Log Out</button>
+            </div>
+          </div>
         </nav>
       </header>
     </>
---
+++ src/Component/RequireAuth.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const RequireAuth = () => {
+  return (
+    <div>RequireAuth</div>
+  )
+}
+
+export default RequireAuth
